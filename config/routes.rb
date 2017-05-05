47047486355530674a6124fe7e0caf85d317964e
Jon Opteynde <jon.opteynde@gmail.com>
2017-05-05 12:31:02 +1000

    4.3 new Orders controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   resources :products
-  root 'static_pages#index' 
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'static_pages#landing_page' 
 
   get 'static_pages/index'
 
